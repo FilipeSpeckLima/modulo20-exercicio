1ed96f025a4ec5c2c745e7b2eb4572ee8d3233e5
Filipe Speck <filipespeck10@gmail.com>
2025-07-22 09:42:58 -0300

    Commit módulo 20

---
+++ mdc.js
@@ -0,0 +1,12 @@
+console.log(mdc(24, 9))
+
+function calcularMDC(a, b) {
+  while (b !== 0) {
+    let resto = a % b;
+    a = b;
+    b = resto;
+  }
+  return a;
+}
+
+console.log(calcularMDC(48, 18)); 
